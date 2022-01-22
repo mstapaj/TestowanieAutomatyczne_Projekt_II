cd9f570fc1b83f77d9c0fb2a15228414eb665abf
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 23:09:22 +0100

    Dodanie testów funkcji edit_item_in_database

--- tests/test_item.py
+++ tests/test_item.py
@@ -17,3 +17,13 @@ class TestItem(unittest.TestCase):
         self.item.add_item_to_database = Mock(side_effect=[None, ValueError('Podany obiekt jest już w bazie')])
         self.item.add_item_to_database()
         assert_that(self.item.add_item_to_database).raises(ValueError)
+
+    def test_edit_item_in_database(self):
+        self.item.edit_item_in_database = Mock(return_value='Pomyślnie edytowano przedmiot')
+        assert_that(self.item.edit_item_in_database(2, 3, 'Latarka', 20.99)).is_equal_to(
+            'Pomyślnie edytowano przedmiot')
+
+    def test_edit_item_in_database_two_time(self):
+        self.item.edit_item_in_database = Mock(side_effect=[None, ValueError('Stary i nowy obiekt są identyczne')])
+        self.item.edit_item_in_database(2, 2, 'Latarka', 20.99)
+        assert_that(self.item.edit_item_in_database).raises(ValueError)
