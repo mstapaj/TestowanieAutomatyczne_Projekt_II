1f9a307a1cc274109bea4003adc56980d4c0f14c
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 21:02:24 +0100

    Dodanie walidacji w item

--- src/item.py
+++ src/item.py
@@ -1,5 +1,11 @@
 class Item:
     def __init__(self, id, name, value):
+        if not isinstance(id,int) or id<0 or str(id)=='True' or str(id)=='False':
+            raise ValueError('Id musi być dodatnią liczbą całkowitą')
+        if not isinstance(name,str):
+            raise ValueError('Nazwa musi być typu string')
+        if not isinstance(id,float) or id<0 or str(id)=='True' or str(id)=='False':
+            raise ValueError('Wartość musi być dodatnią liczbą zmiennoprzecinkową')
         self.id = id
         self.name = name
         self.value = value
