5d9e54a168ba430f158ab134fe78bdd99520668e
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 22:50:35 +0100

    Poprawki w init item

--- src/item.py
+++ src/item.py
@@ -1,10 +1,10 @@
 class Item:
     def __init__(self, id, name, value):
-        if not isinstance(id,int) or id<0 or str(id)=='True' or str(id)=='False':
+        if not isinstance(id, int) or id < 0 or str(id) == 'True' or str(id) == 'False':
             raise ValueError('Id musi być dodatnią liczbą całkowitą')
-        if not isinstance(name,str):
+        if not isinstance(name, str):
             raise ValueError('Nazwa musi być typu string')
-        if not isinstance(id,float) or id<0 or str(id)=='True' or str(id)=='False':
+        if not isinstance(value, float) or value < 0 or str(value) == 'True' or str(value) == 'False':
             raise ValueError('Wartość musi być dodatnią liczbą zmiennoprzecinkową')
         self.id = id
         self.name = name
