18c88242d0e5f41cbea8f166ee45902001a26019
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 19:50:25 +0100

    Dodanie inicjalizacji w order

--- src/client.py
+++ src/client.py
@@ -1,5 +1,6 @@
 class Client:
-    def __init__(self, firstname, lastname, email):
+    def __init__(self, id, firstname, lastname, email):
+        self.id = id
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
---
+++ src/order.py
@@ -0,0 +1,4 @@
+class Order:
+    def __init__(self, id):
+        self.id = id
+        self.items = []
