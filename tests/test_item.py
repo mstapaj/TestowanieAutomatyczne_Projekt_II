5d3043fbfcc4d39a5d4fbadf51d6c031c3650260
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 22:30:07 +0100

    Dodanie testów add_item, poprawki w init item

---
+++ tests/test_item.py
@@ -0,0 +1,25 @@
+import unittest
+from assertpy import assert_that
+from unittest.mock import Mock
+from src.item import Item
+from src.database import Database
+
+
+class TestItem(unittest.TestCase):
+
+    def setUp(self):
+        self.database = Database()
+        self.item = Item(1, 'Piłka', 12.5)
+
+    def test_add_item(self):
+        self.database.add_item = Mock(return_value='Dodano przedmiot do bazy danych')
+        assert_that(self.database.add_item(self.item)).is_equal_to('Dodano przedmiot do bazy danych')
+
+    def test_add_item_wrong_type(self):
+        self.database.add_item = Mock(side_effect=ValueError('Podany obiekt nie jest przedmiotem'))
+        assert_that(self.database.add_item).raises(ValueError).when_called_with(None)
+
+    def test_add_item_two_time(self):
+        self.database.add_item = Mock(side_effect=[None, ValueError('Podany obiekt jest już w bazie')])
+        self.database.add_item(self.item)
+        assert_that(self.database.add_item).raises(ValueError).when_called_with(self.item)
