4e4ed7e70c4a058dd406c932f9ef11156fca707c
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 21:25:23 +0100

    Dodanie walidacji order

--- src/order.py
+++ src/order.py
@@ -1,5 +1,9 @@
 class Order:
     def __init__(self, id, id_client):
+        if not isinstance(id,int) or id<0 or str(id)=='True' or str(id)=='False':
+            raise ValueError('Id musi być dodatnią liczbą całkowitą')
+        if not isinstance(id_client,int) or id_client<0 or str(id_client)=='True' or str(id_client)=='False':
+            raise ValueError('Id klienta musi być dodatnią liczbą całkowitą')
         self.id = id
         self.id_client = id_client
         self.items = []
