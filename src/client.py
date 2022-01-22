6c55d5afbef50e59c2db474e89376b316f26b57b
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 19:48:37 +0100

    Dodanie inicjalizacji w client

---
+++ src/client.py
@@ -0,0 +1,6 @@
+class Client:
+    def __init__(self, firstname, lastname, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.orders = []
