26e218a2ebfb81bcdb78796e9abe9055811bddf9
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-22 23:40:40 +0100

    Dodanie testów parametrycznych dla edit_item_in_database

--- tests/test_parametrized_item.py
+++ tests/test_parametrized_item.py
@@ -38,3 +38,89 @@ class TestParametrizedItem(unittest.TestCase):
 
     def test_item_init_wrong_id_name_value(self):
         assert_that(Item).raises(ValueError).when_called_with(self.wrongId, self.wrongName, self.wrongValue)
+
+
+@parameterized_class(('wrongId', 'wrongNewId', 'wrongName', 'wrongValue'), [
+    ({}, {}, {}, {}),
+    ([], [], [], []),
+    (True, True, True, True),
+    (False, False, False, False),
+    ("abc", 'abc', 2, "abc"),
+    ("", '', -2, ""),
+    (3.13, 2.12, 3.13, 'def'),
+    (-2.56, -2.12, -2.82, -3.25),
+    (-4, -4, -4, -4),
+    (None, 2.1, 2, 'aaa')
+])
+class TestParametrizedEditItemToDatabase(unittest.TestCase):
+
+    def setUp(self):
+        self.temp = Item(1, 'Piłka', 2.5)
+
+    def test_edit_item_in_database_wrong_id(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, 4, 'Latarka',
+                                                                                         18.5)
+
+    def test_edit_item_in_database_wrong_new_id(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(1, self.wrongNewId, 'Latarka',
+                                                                                         18.5)
+
+    def test_edit_item_in_database_wrong_name(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(1, 4, self.wrongName,
+                                                                                         18.5)
+
+    def test_edit_item_in_database_wrong_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(1, 4, 'Latarka',
+                                                                                         self.wrongValue)
+
+    def test_edit_item_in_database_wrong_id_new_id(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, self.wrongNewId,
+                                                                                         'Latarka',
+                                                                                         18.5)
+
+    def test_edit_item_in_database_wrong_new_id_name(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(1, self.wrongNewId,
+                                                                                         self.wrongName,
+                                                                                         18.5)
+
+    def test_edit_item_in_database_wrong_name_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(1, 4, self.wrongName,
+                                                                                         self.wrongValue)
+
+    def test_edit_item_in_database_wrong_id_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, 4, 'Latarka',
+                                                                                         self.wrongValue)
+
+    def test_edit_item_in_database_wrong_id_name(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, 4,
+                                                                                         self.wrongName,
+                                                                                         18.5)
+
+    def test_edit_item_in_database_wrong_new_id_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(1, self.wrongNewId, 'Latarka',
+                                                                                         self.wrongValue)
+
+    def test_edit_item_in_database_wrong_id_new_id_name(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, self.wrongNewId,
+                                                                                         self.wrongName,
+                                                                                         18.5)
+
+    def test_edit_item_in_database_wrong_new_id_name_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(1, self.wrongNewId,
+                                                                                         self.wrongName,
+                                                                                         self.wrongValue)
+
+    def test_edit_item_in_database_wrong_id_name_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, 4,
+                                                                                         self.wrongName,
+                                                                                         self.wrongValue)
+
+    def test_edit_item_in_database_wrong_id_new_id_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, self.wrongNewId,
+                                                                                         'Latarka',
+                                                                                         self.wrongValue)
+
+    def test_edit_item_in_database_wrong_id_new_id_name_value(self):
+        assert_that(self.temp.edit_item_in_database).raises(ValueError).when_called_with(self.wrongId, self.wrongNewId,
+                                                                                         self.wrongName,
+                                                                                         self.wrongValue)
